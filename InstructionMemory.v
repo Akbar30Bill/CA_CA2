49ef0530b247fb03e0e53b6078ffeac16fb9ec4b
Akbar30bill <alishahverdi2010@yahoo.com>
2019-04-13 20:29:31 +0430

    l

---
+++ InstructionMemory.v
@@ -0,0 +1,6 @@
+`timescale 1ns / 1ns
+module InstructionMemory(address,instruction);
+  input[15:0] address;
+  output[31:0]instruction;
+  case address begin
+endmodule
---
+++ ProgramCounter.v
@@ -0,0 +1,15 @@
+`timescale 1ns / 1ns
+module ProgramCounter (clk, rst, pc);
+input clk, rst;
+output pc;
+  reg[15:0] pc_reg;
+  always @(posedge clk, posedge rst) begin
+    if (rst) begin
+      pc_reg <= 16'b0;
+    end
+    else begin
+      pc_reg <= pc_reg + 4;
+    end
+  end
+  assign pc <= pc_reg;
+endmodule //
